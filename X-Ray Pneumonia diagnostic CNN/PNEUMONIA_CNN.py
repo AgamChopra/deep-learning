7b99c4178f5f20df408ea73f959dde24d33eb903
Agam <agamacdc@gmail.com>
2021-01-21 18:59:20 -0500

    test model CNN algorithm added

--- X-Ray Pneumonia diagnostic CNN/PNEUMONIA_CNN.py
+++ X-Ray Pneumonia diagnostic CNN/PNEUMONIA_CNN.py
@@ -1,8 +1,14 @@
 import pandas as pd
 import numpy as np
 from PIL import Image
-from matplotlib import image
-from matplotlib import pyplot
+#from matplotlib import image
+from matplotlib import pyplot as plt
+from keras.layers import Input, Dense, Activation, ZeroPadding2D, BatchNormalization, Flatten, Conv2D
+from keras.layers import MaxPooling2D
+from keras.models import Model
+import keras.backend as K
+K.set_image_data_format('channels_last')
+import tensorflow as tf
 
 def load_set_index():
     
@@ -17,18 +23,18 @@ def load_set_index():
 
 def load_image_as_array(set_name,scan_type,i):
     
-    image = np.zeros((400,400))
+    imagee = np.zeros((400,400))
     
     if set_name == "val":
         
             if scan_type == 0:    
-                    image = np.asarray(Image.open(r'chest_xray\chest_xray\val\NORMAL\img (%d).jpeg' % (i+1)).convert("L"))             
+                    imagee = np.asarray(Image.open(r'chest_xray\chest_xray\val\NORMAL\img (%d).jpeg' % (i+1)).convert("L"))             
             
             elif scan_type == 1:   
-                    image = np.asarray(Image.open(r'chest_xray\chest_xray\val\PNEUMONIA\BACTERIA\img (%d).jpeg' % (i+1)).convert("L"))             
+                    imagee = np.asarray(Image.open(r'chest_xray\chest_xray\val\PNEUMONIA\BACTERIA\img (%d).jpeg' % (i+1)).convert("L"))             
             
             elif scan_type == 2:     
-                    image = np.asarray(Image.open(r'chest_xray\chest_xray\val\PNEUMONIA\VIRUS\img (%d).jpeg' % (i+1)).convert("L"))               
+                    imagee = np.asarray(Image.open(r'chest_xray\chest_xray\val\PNEUMONIA\VIRUS\img (%d).jpeg' % (i+1)).convert("L"))               
             
             else:
                 print("Error code LI1... Unable to load image, incorrect scan type.")            
@@ -36,13 +42,13 @@ def load_image_as_array(set_name,scan_type,i):
     elif set_name == "test":
         
             if scan_type == 0:
-                    image = np.asarray(Image.open(r'chest_xray\chest_xray\test\NORMAL\img (%d).jpeg' % (i+1)).convert("L"))             
+                    imagee = np.asarray(Image.open(r'chest_xray\chest_xray\test\NORMAL\img (%d).jpeg' % (i+1)).convert("L"))             
             
             elif scan_type == 1:
-                    image = np.asarray(Image.open(r'chest_xray\chest_xray\test\PNEUMONIA\BACTERIA\img (%d).jpeg' % (i+1)).convert("L"))             
+                    imagee = np.asarray(Image.open(r'chest_xray\chest_xray\test\PNEUMONIA\BACTERIA\img (%d).jpeg' % (i+1)).convert("L"))             
             
             elif scan_type == 2:
-                    image = np.asarray(Image.open(r'chest_xray\chest_xray\test\PNEUMONIA\VIRUS\img (%d).jpeg' % (i+1)).convert("L"))     
+                    imagee = np.asarray(Image.open(r'chest_xray\chest_xray\test\PNEUMONIA\VIRUS\img (%d).jpeg' % (i+1)).convert("L"))     
             
             else:
                 print("Error code LI1... Unable to load image, incorrect scan type.")                     
@@ -50,13 +56,13 @@ def load_image_as_array(set_name,scan_type,i):
     elif set_name == "train":
         
             if scan_type == 0:
-                    image = np.asarray(Image.open(r'chest_xray\chest_xray\train\NORMAL\img (%d).jpeg' % (i+1)).convert("L"))
+                    imagee = np.asarray(Image.open(r'chest_xray\chest_xray\train\NORMAL\img (%d).jpeg' % (i+1)).convert("L"))
             
             elif scan_type == 1:
-                    image = np.asarray(Image.open(r'chest_xray\chest_xray\train\PNEUMONIA\BACTERIA\img (%d).jpeg' % (i+1)).convert("L"))             
+                    imagee = np.asarray(Image.open(r'chest_xray\chest_xray\train\PNEUMONIA\BACTERIA\img (%d).jpeg' % (i+1)).convert("L"))             
             
             elif scan_type == 2:     
-                    image = np.asarray(Image.open(r'chest_xray\chest_xray\train\PNEUMONIA\VIRUS\img (%d).jpeg' % (i+1)).convert("L"))        
+                    imagee = np.asarray(Image.open(r'chest_xray\chest_xray\train\PNEUMONIA\VIRUS\img (%d).jpeg' % (i+1)).convert("L"))        
             
             else:
                 print("Error code LI1... Unable to load image, incorrect scan type.")                      
@@ -64,7 +70,7 @@ def load_image_as_array(set_name,scan_type,i):
     else:
         print("Error code LI0... Unable to load image, incorrect set name.")
          
-    return(image)
+    return(imagee)
 
 def load_norm_img(set_name,scan_type,i):
     return(load_image_as_array(set_name,scan_type,i)/255)
@@ -93,79 +99,55 @@ def make_tags(set_name):
         y_val_v = populate_index(2*np.ones((index[8],2),dtype=int))
         y_set = np.concatenate((y_val_n,y_val_b,y_val_v),axis=0)
     else:
-        print("Error in make_tag(): Incorrect set name '%s'\nmake_tag() returned 'None'" % (setname))
+        print("Error in make_tag(): Incorrect set name '%s'\nmake_tag() returned 'None'" % (set_name))
         return None
     return y_set
 
 def make_xy_sets(set_name):
     y_temp = make_tags(set_name)
     y_set = np.zeros((y_temp[:,0].size))
-    x_set = np.zeros((y_temp[:,0].size,400,400))
+    x_set = np.zeros((y_temp[:,0].size,400,400,1))
     
     np.random.shuffle(y_temp)
     
     for i in range(y_temp[:,0].size):
         y_set[i] = y_temp[i,1]
-        x_set[i,:,:] = load_norm_img(set_name,y_temp[i,1],0)   
+        x_set[i,:,:,0] = load_norm_img(set_name,y_temp[i,1],0)   
     return x_set,y_set
 
 #print(make_xy_sets("val"))
 
-"""
+X_train,Y_train = make_xy_sets("train")
+print(X_train.shape, X_train.shape[1:4])
+plt.imshow(X_train[0,:,:,0],cmap = "bone")
 
-def load_image_set(train_n,train_b,train_v,test_n,test_b,test_v,val_n,val_b,val_v):
-    val_n_array = np.zeros((400,400,val_n))
-    val_b_array = np.zeros((400,400,val_b))
-    val_v_array = np.zeros((400,400,val_v))
+def TestCNN(input_shape,classes = 3):
     
-    test_n_array = np.zeros((400,400,test_n))
-    test_b_array = np.zeros((400,400,test_b))
-    test_v_array = np.zeros((400,400,test_v))
-    
-    train_n_array = np.zeros((400,400,train_n))
-    train_b_array = np.zeros((400,400,train_b))
-    train_v_array = np.zeros((400,400,train_v))
+    X_input = Input(input_shape)
+    X = ZeroPadding2D((3, 3))(X_input)
+    X = Conv2D(32, (7, 7), strides = (1, 1), name = 'conv0')(X)
+    X = BatchNormalization(axis = 3, name = 'bn0')(X)
+    X = Activation('relu')(X)
+    X = MaxPooling2D((2, 2), name='max_pool')(X)
+    X = Flatten()(X)
+    X = Dense(classes, activation='softmax', name='fc' + str(classes))(X)
+	
+    model = Model(inputs = X_input, outputs = X, name='testmodel')
+	
+    return model
+X_train,Y_train = make_xy_sets("train")
+Y_train = tf.keras.utils.to_categorical(Y_train, 3)
+print(Y_train.shape)
+testmodel = TestCNN(X_train.shape[1:4],classes=3)
+testmodel.compile(optimizer = "Adam", loss = "categorical_crossentropy", metrics = ["accuracy"])
 
+testmodel.summary()
 
-    for i in range(val_n):
-        val_n_array[:,:,i] = np.asarray(Image.open(r'chest_xray\chest_xray\val\NORMAL\img (%d).jpeg' % (i+1)).convert("L"))
-    for i in range(val_b):
-        val_b_array[:,:,i] = np.asarray(Image.open(r'chest_xray\chest_xray\val\PNEUMONIA\BACTERIA\img (%d).jpeg' % (i+1)).convert("L"))    
-    for i in range(val_v):
-        val_v_array[:,:,i] = np.asarray(Image.open(r'chest_xray\chest_xray\val\PNEUMONIA\VIRUS\img (%d).jpeg' % (i+1)).convert("L"))
-    for i in range(test_n):
-        test_n_array[:,:,i] = np.asarray(Image.open(r'chest_xray\chest_xray\test\NORMAL\img (%d).jpeg' % (i+1)).convert("L"))
-    for i in range(test_b):
-        test_b_array[:,:,i] = np.asarray(Image.open(r'chest_xray\chest_xray\test\PNEUMONIA\BACTERIA\img (%d).jpeg' % (i+1)).convert("L"))    
-    for i in range(test_v):
-        test_v_array[:,:,i] = np.asarray(Image.open(r'chest_xray\chest_xray\test\PNEUMONIA\VIRUS\img (%d).jpeg' % (i+1)).convert("L"))
-    for i in range(train_n):
-        train_n_array[:,:,i] = np.asarray(Image.open(r'chest_xray\chest_xray\train\NORMAL\img (%d).jpeg' % (i+1)).convert("L"))
-    for i in range(train_b):
-        train_b_array[:,:,i] = np.asarray(Image.open(r'chest_xray\chest_xray\train\PNEUMONIA\BACTERIA\img (%d).jpeg' % (i+1)).convert("L"))    
-    for i in range(train_v):
-        train_v_array[:,:,i] = np.asarray(Image.open(r'chest_xray\chest_xray\train\PNEUMONIA\VIRUS\img (%d).jpeg' % (i+1)).convert("L"))
-              
-    return  (val_n_array,val_b_array,val_v_array,test_n_array,test_b_array,test_v_array,train_n_array,train_b_array,train_v_array)
+testmodel.fit(x = X_train, y = Y_train, epochs = 1, batch_size = 24)
 
-def load_data_sets():
-    # setup
-    index = load_image_data()
-    
-    x_train = np.zeros((400,400,index[0]+index[1]+index[2]))
-    y_train = np.zeros((index[0]+index[1]+index[2]))
-    
-    x_test = np.zeros((400,400,index[3]+index[4]+index[5]))
-    y_test = np.zeros((index[3]+index[4]+index[5]))
-    
-    x_val = np.zeros((400,400,index[6]+index[7]+index[8]))
-    y_val = np.zeros((index[6]+index[7]+index[8]))
-    
-    n_v,b_v,v_v,n_ts,b_ts,v_ts,n_tr,b_tr,v_tr = load_image_set(index[0],index[1],index[2],index[3],index[4],index[5],index[6],index[7],index[8])
-    print(n_v.shape)
-    return x_train,y_train,x_test,y_test,x_val,y_val
-  
-x_train,y_train,x_test,y_test,x_val,y_val = load_data_sets()
-print(x_train.shape,y_train.shape,x_test.shape,y_test.shape,x_val.shape,y_val.shape)
-print(x_val,y_val)
-"""
+X_test,Y_test = make_xy_sets("test")
+Y_test = tf.keras.utils.to_categorical(Y_test, 3)
+print(Y_test.shape)
+preds = testmodel.evaluate(x = X_test, y = Y_test)
+
+print ("\nLoss = " + str(preds[0]) + "\nTest Accuracy = " + str(preds[1]))
