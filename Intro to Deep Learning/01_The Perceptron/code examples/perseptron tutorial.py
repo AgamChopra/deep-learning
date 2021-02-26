20a32bebba200c8093408ecf532a030bcafe2d1d
Agam main <work.agamchopra@gmail.com>
2021-02-26 11:46:41 -0500

    updated code for perceptron

--- Intro to Deep Learning/01_The Perceptron/code examples/perseptron tutorial.py
+++ Intro to Deep Learning/01_The Perceptron/code examples/perseptron tutorial.py
@@ -1,4 +1,4 @@
-# ©[2021][Agamdeep Chopra]
+# Â©[2021][Agamdeep Chopra]
 # Feel free to use this code for learning purposes but please properly credit me in APA format.
 	# Example 1, Perceptron Binary Inverter
 	# Suppliment to my Introduction to Deep Learning Video 1
@@ -16,8 +16,8 @@ from matplotlib import pyplot as plt
 
 #%%
 #Define the hyperparameters
-η = 0.001 #learning rate. Dont worry aboput this for now.
-ε = 1E-9 #very small value constant. Dont worry aboput this for now.
+alpha = 0.001 #learning rate. Dont worry aboput this for now.
+eta = 1E-9 #very small value constant. Dont worry aboput this for now.
 
 #Define the inputs and ground truth values(expected outputs)
 X = np.array((1,0)) # input
@@ -34,7 +34,7 @@ def percp_parm():
 def loss(yp,ye):
     return (np.square(ye-yp))
 
-#Average the losses and divide by 2 to get cost. Dont worry about the 1/2 factor for now. Vola! You have Mean Square Error!
+#Dont worry about cost for now. We wont use it for this example.
 def cost(yp,ye):
     return (np.mean(loss(yp,ye))/2)
 
@@ -51,68 +51,76 @@ def fprop(x,w,b):
 # by ever so slightly changing the values of the parameters in the direction of the-
 # minima (local or global).
 #Do not worry about optimizing the gradient decent process for now.
-def bprop(η,x,yp,ye,w,b):
-    Δw,Δb = diff(x,yp,ye,w,b) #calculate Δx and Δy
-    w_new = w - η*Δw #update weight
-    b_new = b - η*Δb #update bias
-    return (w_new,b_new)
+def bprop(alpha,x,yp,ye,w,b):
+    dw,db = diff(x, yp, ye, w, b) #calculate dx and dy
+    w = w - alpha*dw #update weight
+    b = b - alpha*db #update bias
+    #print("w back",w.shape,"b back",b.shape)
+    return (w,b)
 
-#Calculate the Δ values using chain rule on Loss J and partial derivatives.
+#Calculate the delta values using chain rule on Loss J and partial derivatives.
 def diff(x,yp,ye,w,b):
-    Δw = ((cost(yp+ε,ye)-cost(yp-ε,ye))/(2*ε))*((fprop(x,w+ε,b)-fprop(x,w-ε,b))/(2*ε))
-    # Δw <- ∂J/∂w = (∂J/∂y)(∂y/∂w)
-    #             = ((J(y+ε)-J(y-ε))/2ε)*((y(w+ε)-y(w-ε))/2ε) {where y = actiation(linear(x,w,b))
-    #                                                                J = Loss(y,y_exp)           }
-    Δb = ((cost(yp+ε,ye)-cost(yp-ε,ye))/(2*ε))*((fprop(x,w,b+ε)-fprop(x,w,b-ε))/(2*ε))
-    # Δb <- ∂J/∂b = (∂J/∂y)(∂y/∂b)
-    #             = ((J(y+ε)-J(y-ε))/2ε)*((y(b+ε)-y(b-ε))/2ε) {where y = actiation(linear(x,w,b))
-    #                                                                J = Loss(y,y_exp)           }
-    return (Δw,Δb)
+    dw = ((loss(yp+eta,ye)-loss(yp-eta,ye))/(eta))*((fprop(x,w+eta,b)-fprop(x,w-eta,b))/(2*eta))
+    # dw <- dJ/dw = (dJ/dy)(dy/dw)
+    #             = ((J(y+eta)-J(y-eta))/2eta)*((y(w+eta)-y(w-eta))/2eta) {where y = actiation(linear(x,w,b))
+    #                                                                            J = Loss(y,y_exp)           }
+    db = ((loss(yp+eta,ye)-loss(yp-eta,ye))/(eta))*((fprop(x,w,b+eta)-fprop(x,w,b-eta))/(2*eta))
+    # db <- dJ/db = (dJ/dy)(dy/db)
+    #             = ((J(y+eta)-J(y-eta))/2eta)*((y(b+eta)-y(b-eta))/2eta) {where y = actiation(linear(x,w,b))
+    #                                                                            J = Loss(y,y_exp)           }
+    #print("dw diff",dw.shape,"db diff",db.shape)
+    return (dw,db)
 
 #Putting it all together in the Perceptron model
 #Epoch is the number of times we want to run the model with backprop to minimize J.
 def perceptron(x,ye,epoch=100): 
     w,b = percp_parm() #initializing the parameters
+    #print("w initial ",w.shape,"b initial ",b.shape)
     ls = []
     list_index = 0
     for i in range(epoch):#loop to minimize J.
-        #Using vectorization properties of numpy:
-        yp = fprop(x, w, b)#the forward step to calculate the output
-        w,b = bprop(η, x, yp, ye, w, b)#update the parameters.
-        #loss calculation for visualization. Dont worry about the next 3 lines
-        if i%500 == 0:
-            ls.append(cost(yp, ye))
-            print(ls[list_index])
-            list_index += 1
+        for j in range(len(x)):#in future, we will use mini batches and vectorization properties of numpy
+            yp = fprop(x[j], w, b)#the forward step to calculate the output
+            #print("yp fprop after ",yp.shape)
+            w,b = bprop(alpha, x[j], yp, ye[j], w, b)#update the parameters.
+            #print("w after ",w.shape,"b after ",b.shape)
+            #loss calculation for visualization. Dont worry about the next 3 lines
+            if i%10000 == 0 and j == 1:
+                ls.append(loss(yp, ye[j]))
+                print("Loss", ls[list_index])
+                list_index += 1
     return w,b,ls
 #%%
 #Finally! We are all set!
 #Lets try running the model.
 #If everything works properly, you should see something like:
-#[-0.62291858] [0.]
 #[0.18596151]
 #[0.12121284]
 #....
 #[0.00908715]
 #[0.00857715]
 #optimized weight =  [-4.58871226]  and bias =  [2.17494229]
-#if x = 0 then y_pred =  [1.]  with value [0.89797664]  at threshold 0.5
-#if x = 1 then y_pred =  [0.]  with value [0.08212868]  at threshold 0.5
-#here, as long as our model spits out a value larger than 1, we say that the prediction is 1 and vice versa.
-w,b = perceptron(X, Y_exp,epoch = 10000) 
+w,b,J = perceptron(X, Y_exp,epoch = 100000)
 print("optimized weight = ",w," and bias = ",b)
+#%%
 #testing the solution
+#here, as long as our model returns a value larger than 0.5, we say that the prediction is 1 and vice versa.
 print("if x = 0 then y_pred = ",np.around(fprop(0, w, b))," with value",fprop(0, w, b)," at threshold 0.5")
 print("if x = 1 then y_pred = ",np.around(fprop(1, w, b))," with value",fprop(1, w, b)," at threshold 0.5")
+plt.plot(J, color='red',linewidth=1)
+plt.xlabel('Epoch(x10,000)')
+plt.ylabel('Loss')
+plt.title('Loss of Binary I/O Perceptron during training')
+plt.show()
 #%%
 #pre-trained model parameters and loss for visualization.
-ws,bs = [-4.54522017,2.15276728] #saved weights
+ws,bs = [-5.39722823,2.58551185] #saved weights
 print("saved weight = ",w," and bias = ",b)
 print("if x = 0 then y_pred = ",np.around(fprop(0, ws, bs))," with value",fprop(0, ws, bs)," at threshold 0.5")
 print("if x = 1 then y_pred = ",np.around(fprop(1, ws, bs))," with value",fprop(1, ws, bs)," at threshold 0.5")
-y = np.array((0.27576416,0.0449583,0.02019229,0.01250398,0.00892285))
-x =  np.array((0,25000,50000,75000,100000))
-plt.plot(x, y, color='red', marker='o', linestyle='dashed',linewidth=1, markersize=4, label='output w=-4.54522017 ,b=2.15276728')
+y = np.array((0.25014776,0.07788755,0.03378257,0.02048394,0.01442323,0.01102979,0.00888375,0.00741332,0.00634711,0.00554078))
+x =  np.array((0,10000,20000,30000,40000,50000,60000,70000,80000,90000))
+plt.plot(x, y, color='red', marker='o', linestyle='dashed',linewidth=1, markersize=4, label='output w=-5.39722823 ,b=2.58551185')
 plt.xlabel('Epoch')
 plt.ylabel('Loss (MSE)')
 plt.title('Loss of Binary I/O Perceptron during training')
